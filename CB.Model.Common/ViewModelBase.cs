4f9e318f8c5e473e019b0770a50397b17c1761a7
Connor <grisetroseciel@gmail.com>
2016-03-07 08:25:04 +0700

    add ViewModelBase 2

---
+++ CB.Model.Common/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.TeamFoundation.MVVM;
+
+
+namespace CB.Model.Common
+{
+    public class ViewModelBase: ObservableObject
+    {
+        #region Implementation
+        protected ICommand GetCommand(ref ICommand command, Action<object> execute, Predicate<object> canExecute)
+        {
+            return command ?? (command = new RelayCommand(execute, canExecute));
+        }
+        #endregion
+    }
+}
